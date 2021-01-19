d0ee6bc0061b84427c66ee94093e3e68c510969c
JakeJazokas <jakejazokas@outlook.com>
2021-01-19 14:10:28 -0500

    Add files via upload

---
+++ EthanFitnessService.py
@@ -0,0 +1,29 @@
+import time
+import random
+import ctypes
+from tkinter import messagebox
+from SMWinservice import SMWinservice
+
+class EthanFitnessService(SMWinservice):
+    _svc_name_ = "EthanFitnessService"
+    _svc_display_name_ = "Ethan Fitness Service"
+    _svc_description_ = "Popup workouts"
+
+    def start(self):
+        self.isrunning = True
+
+    def stop(self):
+        self.isrunning = False
+
+    def main(self):
+        i = 0
+        while self.isrunning:
+            '''
+            LOGIC GOES HERE
+            '''
+            #Check if service is stopped
+            if self.isrunning == False: 
+                break
+
+if __name__ == '__main__':
+    EthanFitnessService.parse_command_line()
---
+++ README.txt
@@ -0,0 +1,11 @@
+To install the service run:
+	- python EthanFitnessService.py install
+
+To remove the service run (After it is stopped):
+	- python EthanFitnessService.py remove
+
+After the service is installed, run the following command to start it:
+	- net start EthanFitnessService
+
+To stop the service, run the following command:
+	- net stop EthanFitnessService
